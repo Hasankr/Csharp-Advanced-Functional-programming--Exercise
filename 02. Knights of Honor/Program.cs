d013058d61980ba52e6f03efefaa4ef9b45aab3e
hasankr <hasankr@abv.bg>
2022-10-03 20:31:31 +0300

    added 2

---
+++ 02. Knights of Honor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printNamesWithAddedTittle = names =>
+            {
+                foreach (var name in names)
+                {
+                    Console.WriteLine($"Sir {name}");
+                }
+            };
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            printNamesWithAddedTittle(names);
+        }
+    }
+}
