c2ab1dfa75454677d8c16e9d72fad77da4d5b9f6
hasankr <hasankr@abv.bg>
2022-10-03 22:19:24 +0300

    added 7

---
+++ 07. Predicate For Names/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[], Predicate<string>> printNames = (names, match) =>
+            {
+                foreach (var name in names)
+                {
+                    if (match(name))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            };
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            printNames(names, n => n.Length <= length);
+        }
+    }
+}
