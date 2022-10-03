330cd2cce6d1dad53adcc24f87d8fd1b1d448c2c
hasankr <hasankr@abv.bg>
2022-10-03 20:43:52 +0300

    added3

---
+++ 03. Custom Min Function/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> min = (numbers) => numbers.Min();
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Console.WriteLine(min(numbers));
+
+        }
+    }
+}
